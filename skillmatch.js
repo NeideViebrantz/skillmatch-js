100a59720a248b4078ab852235216cda861e0374
NeideViebrantz <neideviebrantz@hotmail.com>
2026-05-17 13:05:46 -0300

    Create skillmatch.js
    Primeiro Commit

---
+++ skillmatch.js
@@ -0,0 +1,44 @@
+const candidato = { 
+    nome: "Paula",
+    area:"Front-End",
+    habilidades: ["JavaScript", "GitHub", "Lógica de Programação", "Kanban"],
+experienciaMeses: 5 
+};
+
+const vagas = [
+    {
+        id: 1,
+        empresa: "LetSystens",
+        cargo: "Desenvolvedor Front-End Júnior",
+        requisitos: ["JavaScript", "GitHub", "Lógica de Programação"],
+        salario: 3200,
+        modalidade: "Remoto"
+    },
+
+    { id: 2,
+        empresa: "GreSystens",
+        cargo: "Programador JavaScript Júnior",
+        requisitos: ["JavaScript", "Arrays", "Objetos"],
+        salario: 3500,
+        modalidade: "Híbrido"
+
+    }
+    { id: 3,
+        empresa: "NjvSistemas",
+        cargo: "Estágio Front-End React",
+        requisitos: ["JavaScript", "Arrays", "React"],
+        salario: 1900,
+        modalidade: "Presencial"
+    }
+
+    { id: 4,
+        empresa: "CodeSystems",
+        cargo: "Desenvolvedor web",
+        requisitos: ["Html", "CSS", "JavaScript"],
+        salario: 2700,
+        modalidade: "Híbrido"
+    }
+];
+
+
+ 
